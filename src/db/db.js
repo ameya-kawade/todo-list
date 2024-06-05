0761de5612e821998f7d30678e43861a4b82274a
Cool-developer-975 <ameyakawade0@gmail.com>
2024-06-05 10:47:41 +0530

    made some minor changes

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
 
 export const connectToDB = async ()=>{
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);
         console.log(connectionInstance.connection.host);
         console.log("Connected to mongodb");
         return connectionInstance;
