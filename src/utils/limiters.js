3b6d36e913dba6c92e6ca82943ab11db36cc77cf
Cool-developer-975 <ameyakawade0@gmail.com>
2024-06-21 22:03:13 +0530

    made some minor changes

--- src/utils/limiters.js
+++ src/utils/limiters.js
@@ -2,21 +2,21 @@ import { rateLimit } from 'express-rate-limit';
 
 
 export const Getlimiter = rateLimit({
-    windowMs: 12 * 60 * 60 * 1000,
-    limit: 50,
-    message: 'You have reached the limit to access the page try again after 12 hours'
+    windowMs: 60 * 60 * 1000,
+    limit: 100,
+    message: 'You have reached the limit to access the page try again after 3 hours'
 });
 
 
 export const PostLimiter = rateLimit({
     windowMs: 3 * 60 * 60 * 1000,
-    limit: 5,
+    limit: 10,
     message: 'You have reached the limit try again after 3 hours'
 });
 
 export const BlogLimiter = rateLimit({
-    windowMs: 6 * 60 * 60 * 1000,
+    windowMs: 2 * 60 * 60 * 1000,
     limit: 100,
-    message: 'You have reached the limit to access the page try again after 6 hours'
+    message: 'You have reached the limit to access the page try again after 3 hours'
 });
 
