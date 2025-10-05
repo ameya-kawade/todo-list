5c93a41f251e6738be740f81f33c5d614aeeb220
ameya-kawade <ameyakawade0@gmail.com>
2025-10-05 10:22:52 +0530

    made some minor changes

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { __dirname } from '../path.js';
 import { join } from "path";
 
 export const app = express();
-app.set('trust proxy', true);
+// app.set('trust proxy', true);
 app.use('/static',express.static( join(__dirname, 'public') ));
 app.use(express.urlencoded({extended:false,limit:'16kb'}));
 app.use(express.json({limit:'16kb'}));
